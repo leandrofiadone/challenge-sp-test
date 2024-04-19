55bba68aac6b85b2a7d0debba4712f830289f7a1
Leandro Fiadone <leanfiadone@gmail.com>
2024-04-19 01:49:53 -0300

    baseURL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,8 @@ import Notification from "./Notification"
 import "./App.css" // Import CSS file for styling
 
 
+const baseUrl = "https://challenge-sp-test-8.onrender.com"
+
 const App: React.FC = () => {
   const [showNotification, setShowNotification] = useState(false)
   const [users, setUsers] = useState<User[]>([])
@@ -17,7 +19,7 @@ const App: React.FC = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `http://localhost:3000/api/users?q=${searchTerm}`
+          `${baseUrl}/api/users?q=${searchTerm}`
         )
         const data = await response.json()
         setUsers(data.data)
@@ -44,7 +46,7 @@ const App: React.FC = () => {
       const formData = new FormData()
       if (selectedFile) {
         formData.append("file", selectedFile)
-        const response = await fetch("http://localhost:3000/api/files", {
+        const response = await fetch(`${baseUrl}`, {
           method: "POST",
           body: formData
         })
@@ -96,61 +98,3 @@ const App: React.FC = () => {
 
 export default App
 
-// import React, {useState, useEffect} from "react"
-// import Upload from "./components/Upload"
-// import Card from "./components/Card"
-// import Search from "./components/Search"
-// import {User} from "./interfaces"
-
-// const App: React.FC = () => {
-//   const [users, setUsers] = useState<User[]>([])
-
-//   // Fetch data on component mount
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const response = await fetch("/api/users") // Replace with your API endpoint
-//       const data = await response.json()
-//       setUsers(data)
-//     }
-
-//     fetchData()
-//   }, [])
-
-//   const handleUpload = (newUsers: User[]) => {
-//     setUsers(newUsers)
-//   }
-
-// const filteredUsers = users.filter((user) => {
-//   const searchTerm = /* Get search term from Search component state */;
-
-//   if (!searchTerm) {
-//     return true; // Show all users if no search term
-//   }
-
-//   // Implement case-insensitive search using toLowerCase()
-//   const lowerSearchTerm = searchTerm.toLowerCase();
-//   const lowerName = user.name.toLowerCase();
-
-//   // Search by name (you can add more search criteria as needed)
-//   return lowerName.includes(lowerSearchTerm);
-// });
-
-//   return (
-//     <div className="App">
-//       <h1>CSV Data Viewer</h1>
-//       <Upload onUpload={handleUpload} />
-//       <Search />
-//       {filteredUsers.length > 0 ? (
-//         <div className="cards">
-//           {filteredUsers.map((user) => (
-//             <Card key={user.name} user={user} /> // Pass user as props
-//           ))}
-//         </div>
-//       ) : (
-//         <p>No users uploaded or found in search.</p>
-//       )}
-//     </div>
-//   )
-// }
-
-// export default App
