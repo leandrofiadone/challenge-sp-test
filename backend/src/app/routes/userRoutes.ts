618ee35e15f1df60aa42890c8e71a674b9286567
Leandro <leanfiadone@gmail.com>
2024-05-07 20:01:43 -0300

    reorganizacion

--- backend/src/app/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-// src/app/routes/userRoutes.ts
-import express from "express"
-import {searchUsers} from "../controllers/userController"
-
-const router = express.Router()
-
-router.get("/api/users", searchUsers)
-
-export default router
