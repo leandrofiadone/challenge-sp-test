a3d7adcdc02ed9d3784ae23cb76c76e3839f2a52
Leandro <leanfiadone@gmail.com>
2024-05-07 21:18:33 -0300

    creador

--- frontend/src/view/Creador.tsx
+++ frontend/src/view/Creador.tsx
@@ -1,41 +1,11 @@
-import React, {useState} from "react"
-import {GoogleSpreadsheet} from "google-spreadsheet"
 
-const CLIENT_ID: string = "TU_ID_DE_CLIENTE"
-const API_KEY: string = "TU_API_KEY"
-const SPREADSHEET_ID: string = "TU_ID_DE_HOJA_DE_CÁLCULO"
 
 function Creador(): JSX.Element {
-  const [csvData, setCsvData] = useState<string>("")
 
-  const handleCreateCSV = async (): Promise<void> => {
-    try {
-      const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
-      await doc.useApiKey(API_KEY)
-      await doc.loadInfo()
-
-      const sheet = doc.sheetsByIndex[0] // Suponiendo que es la primera hoja
-      const rows = await sheet.getRows()
-
-      const csvContent: string = rows
-        .map((row: { _rawData: { [ s: string ]: unknown } | ArrayLike<unknown> }) => Object.values(row._rawData).join(","))
-        .join("\n")
-      setCsvData(csvContent)
-    } catch (error) {
-      console.error("Error:", error)
-    }
-  }
 
   return (
     <div>
-      <button onClick={handleCreateCSV}>Crear CSV</button>
-      {csvData && (
-        <a
-          href={`data:text/csv;charset=utf-8,${encodeURIComponent(csvData)}`}
-          download="datos.csv">
-          Descargar CSV
-        </a>
-      )}
+      <p>Hola</p>
     </div>
   )
 }
